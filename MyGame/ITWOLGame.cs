f33d9351bfa86f5edb0fbf2cb18f412b688e4e41
Your Name <you@example.com>
2017-11-21 01:25:19 +0200

    переименования проекта и компонентов на установленное название "ITWOL" - @In The Web Of Li(f)e"

--- MyGame/ITWOLGame.cs
+++ MyGame/ITWOLGame.cs
@@ -7,7 +7,7 @@ namespace MyGame
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class ITWOLGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
@@ -16,7 +16,7 @@ namespace MyGame
         /// </summary>
         Texture2D testGrid;
 
-        public Game1()
+        public ITWOLGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
