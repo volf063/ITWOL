c4960f3949165dd845817a1898bbfb0128b44a3a
Your Name <you@example.com>
2017-11-21 02:48:39 +0200

    измнение структуры контентной части; создание класса ScreenManager - класс для управления контентом на экране, если выражаться грубо

---
+++ ITWOL/ScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITWOL
+{
+    /// <summary>
+    /// The screen manager is a component which manages one or more GameScreen
+    /// instances. It maintains a stack of screens, calls their Update and Draw
+    /// methods at the appropriate times, and automatically routes input to the
+    /// topmost active screen.
+    /// </summary>
+    public class ScreenManager : DrawableGameComponent
+    {
+        
+        public ScreenManager(Game game) : base(game)
+        {
+        }
+    }
+}
